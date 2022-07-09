f1060add8a4ad882e1933d8c6b2601c9503414d9
kavoom2 <kavoom2@gmail.com>
2022-07-10 04:31:11 +0900

    refactor: index.html 불필요한 소스코드 제거 및 React Helemt로 이전

--- src/pages/home/Head.jsx
+++ src/pages/home/Head.jsx
@@ -7,8 +7,10 @@ function Head() {
       <title>Hey Ms.Choi!</title>
       <meta property="og:url" content="https://hey-ms-choi.com/" />
       <meta property="og:title" content="Hey Ms Choi!" />
+      <meta name="description" content="우리의 예쁜 추억을 보여 드릴게요!" />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={thumbnail} />
+      <meta name="theme-color" content="#e8cd76" />
       <meta
         property="og:description"
         content="우리의 예쁜 추억을 보여 드릴게요!"
